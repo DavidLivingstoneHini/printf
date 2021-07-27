cd61a778fea74ad6218fa855c8bfcb7e4ed9a00e
Biniyam Tamene <binitame123@gmail.com>
2021-07-27 05:59:05 -0700

    Printf commit

--- _printf.c
+++ _printf.c
@@ -1,4 +1,38 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 #include "holberton.h"
+
+/**
+ * find_char_func - func that finds formats
+ * @format: format
+ * Return: NULL
+ */
+int (*find_char_func(const char *format))(va_list)
+{
+unsigned int i = 0;
+find_f c[] = {
+{"c", print_char},
+{"s", print_string},
+{"d", print_dec},
+{"i", printf_int},
+{"b", printf_bin},
+{"r", print_rev},
+{"u", print_unsigned},
+{"o", print_octal},
+{"x", print_hex},
+{"X", print_HEX},
+{"R", print_rot13},
+{NULL, NULL}
+};
+while (c[i].sc)
+{
+if (c[i].sc[0] == (*format))
+return (c[i].f);
+i++;
+}
+return (NULL);
+}
 
 /**
  * _printf - produces output according to a format
@@ -10,34 +44,38 @@
  */
 int _printf(const char *format, ...)
 {
-	register short len = 0;
-	int (*printFunc)(va_list, mods *);
-	mods prefixes = PF_INIT;
-	const char *p = format;
-	va_list arguments;
-
-	va_start(arguments, format);
-	assert(invalidInputs(p));
-	for (; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				len += _putchar('%');
-				continue;
-			}
-			while (get_flags(*p, &prefixes))
-				p++;
-			printFunc = get_print(*p);
-			len += (printFunc)
-				? printFunc(arguments, &prefixes)
-				: _printf("%%%c", *p);
-		} else
-			len += _putchar(*p);
-	}
-	_putchar(FLUSH);
-	va_end(arguments);
-	return (len);
+va_list list;
+int (*f)(va_list);
+unsigned int i = 0, len = 0;
+if (format == NULL)
+return (-1);
+va_start(list, format);
+while (format[i])
+{
+while (format[i] != '%' && format[i])
+{
+_putchar(format[i]);
+len++;
+i++;
+}
+if (format[i] == '\0')
+return (len);
+f = find_char_func(&format[i + 1]);
+if (f != NULL)
+{
+len += f(list);
+i += 2;
+continue;
+}
+if (!format[i + 1])
+return (-1);
+_putchar(format[i]);
+len++;
+if (format[i + 1] == '%')
+i += 2;
+else
+i++;
+}
+va_end(list);
+return (len);
 }
--- holberton.h
+++ holberton.h
@@ -16,6 +16,31 @@
 #define KILOBYTE 1024
 #define ABS(x) (((x) <  0) ? -(x) : (x))
 
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_string(va_list s);
+int printf_int(va_list i);
+int printf_bin(va_list b);
+int print_dec(va_list d);
+int print_unsigned(va_list u);
+int print_octal(va_list o);
+int print_hex(va_list h);
+int print_HEX(va_list H);
+int print_rev(va_list r);
+int print_rot13(va_list R);
+
+/**
+ * struct c_format - structure format
+ * @sc: specifier
+ * @f: func
+ */
+typedef struct c_format
+{
+char *sc;
+int (*f)(va_list);
+} find_f;
+
 /**
  * struct modifiers - struct containing flags to "turn on"
  * when a flag specifier is passed to _printf()
@@ -48,8 +73,6 @@ typedef struct printHandler
 } ph;
 
 /* print_nums */
-int print_int(va_list i);
-void print_number(int n);
 int print_unsigned(va_list l, mods *f);
 int count_digit(int i);
 
