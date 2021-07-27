d9fbc1c9bd247f16badadabf50fb3ccf0b08e92e
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-07-27 16:06:29 +0000

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -1,40 +1,76 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#define INT_DEC_SIZE (sizeof(int) * (CHAR_BIT / 3 + 3))
+
+#include <stdlib.h>
 #include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
-#include <stdint.h>
+
 /**
- * struct print_methods - struct for all print identifiers
- *
- * @fid: the identifier
- * @func: the function to return to print as needed by the identifier
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
  */
-typedef struct print_methods
+typedef struct flags
 {
-	char *fid;
-	int (*func)(va_list list);
-} print_methods;
+	int plus;
+	int space;
+	int hash;
+} flags_t;
 
-int _putchar(char c);
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+/* print_nums */
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+
+/* print_bases */
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* _printf */
 int _printf(const char *format, ...);
-int (*get_func(const char *s))(va_list);
 
-int cb_print_string_r(va_list list);
-int cb_print_string(va_list list);
-int cb_print_char(va_list list);
-int cb_print_integers(va_list list);
-int cb_print_binary(va_list list);
-int cb_print_rot13(va_list list);
+/* get_print */
+int (*get_print(char s))(va_list, flags_t *);
 
-void _itoa(int value, char *str, int base);
-void _unsigneditoa(unsigned int value, char *str, int base);
-void _octalitoa(int value, char *str, int base);
-int cb_print_unsigned_integers(va_list list);
-int cb_print_octal_integers(va_list list);
-int cb_print_hex_integers(va_list list);
-int cb_print_hex_capital_integers(va_list list);
-int cb_print_pointers(va_list list);
-#endif /* HOLBERTON_H */
+/* get_flag */
+int get_flag(char s, flags_t *f);
+
+/* print_alpha */
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
+
+/* print_custom */
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+
+/* print_address */
+int print_address(va_list l, flags_t *f);
+
+/* print_percent */
+int print_percent(va_list l, flags_t *f);
+
+#endif
