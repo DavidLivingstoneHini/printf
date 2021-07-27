d823f091b88b4409cdc24b72ab07017bf9ddece0
Biniyam Tamene <binitame123@gmail.com>
2021-07-27 04:31:32 -0700

    Printf Commit

--- holberton.h
+++ holberton.h
@@ -13,9 +13,9 @@
  */
 typedef struct flags
 {
-	int plus;
-	int space;
-	int hash;
+int plus;
+int space;
+int hash;
 } flags_t;
 
 /**
@@ -24,16 +24,11 @@ typedef struct flags
  * @c: format specifier
  * @f: pointer to the correct printing function
  */
-typedef struct printHandler
-{
-	char c;
-	int (*f)(va_list ap, flags_t *f);
-} ph;
 
 /* print_nums */
-int print_int(va_list l, flags_t *f);
+int print_int(va_list l);
 void print_number(int n);
-int print_unsigned(va_list l, flags_t *f);
+int print_unsigned(va_list l);
 int count_digit(int i);
 
 /* print_bases */
@@ -55,8 +50,8 @@ int (*get_print(char s))(va_list, flags_t *);
 int get_flag(char s, flags_t *f);
 
 /* print_alpha */
-int print_string(va_list l, flags_t *f);
-int print_char(va_list l, flags_t *f);
+int print_string(va_list l);
+int print_char(va_list l);
 
 /* write_funcs */
 int _putchar(char c);
@@ -71,6 +66,6 @@ int print_bigS(va_list l, flags_t *f);
 int print_address(va_list l, flags_t *f);
 
 /* print_percent */
-int print_percent(va_list l, flags_t *f);
+int print_percent(va_list l);
 
 #endif
