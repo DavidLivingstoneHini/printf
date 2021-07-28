e44b2b85f3285ecf8cbeac89eb732cd18162bc64
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-07-28 00:53:10 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,47 +1,28 @@
 #include "holberton.h"
+#include <stdarg.h>
+#include <stdlib.h>
 
 /**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
+ * _printf - printsand formats strings
+ *
+ * @format: format string
+ *
+ * Return: number of charachters printed
  */
+
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	int print_count = 0;
+	va_list arg_list;
 
-	register int count = 0;
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+	va_start(arg_list, format);
+
+	print_count = parser(format, arg_list);
+
+	va_end(arg_list);
+
+	return (print_count);
 }
