8e145edec2991242cecdd822906839234ecf7bd7
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-07-28 12:36:10 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -12,17 +12,38 @@
 
 int _printf(const char *format, ...)
 {
-	int print_count = 0;
-	va_list arg_list;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	if (!format || (format[0] == '%' && format[1] == '\0'))
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
-	va_start(arg_list, format);
-
-	print_count = parser(format, arg_list);
-
-	va_end(arg_list);
-
-	return (print_count);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 }
