1edfc8ce4d83b599417f30aa042dbf0093c4fcbc
Biniyam Tamene <binitame123@gmail.com>
2021-07-28 07:24:30 -0700

    Printf Commit

--- holberton.h
+++ holberton.h
@@ -4,73 +4,29 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-/**
- * struct format_specifiers - struct holing each format specifier with
- * its callback
- * @specifier: the format specifier
- * @printer: callback for printing the argument
- */
-
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
-
-/**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
- */
-typedef struct printHandler
-{
-	char c;
-	int (*f)(va_list ap, flags_t *f);
-} ph;
-
-/* print_nums */
-int print_int(va_list l, flags_t *f);
-void print_number(int n);
-int print_unsigned(va_list l, flags_t *f);
-int count_digit(int i);
-
-/* print_bases */
-int print_hex(va_list l, flags_t *f);
-int print_hex_big(va_list l, flags_t *f);
-int print_binary(va_list l, flags_t *f);
-int print_octal(va_list l, flags_t *f);
-
-/* converter */
-char *convert(unsigned long int num, int base, int lowercase);
-
-/* _printf */
-int _printf(const char *format, ...);
-
-/* get_print */
-int (*get_print(char s))(va_list, flags_t *);
-
-/* get_flag */
-int get_flag(char s, flags_t *f);
-
-/* print_alpha */
-int print_string(va_list l, flags_t *f);
-int print_char(va_list l, flags_t *f);
-
-/* write_funcs */
 int _putchar(char c);
-int _puts(char *str);
+int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_string(va_list s);
+int print_int(va_list i);
+int print_dec(va_list d);
+int print_rev(va_list r);
+int print_bin(va_list b);
+int print_unsigned(va_list u);
+int print_octal(va_list o);
+int print_hex(va_list x);
+int print_HEX(va_list X);
+int print_rot13(va_list R);
 
-/* print_custom */
-int print_rot13(va_list l, flags_t *f);
-int print_rev(va_list l, flags_t *f);
-int print_bigS(va_list l, flags_t *f);
-
-/* print_address */
-int print_address(va_list l, flags_t *f);
-
-/* print_percent */
-int print_percent(va_list l, flags_t *f);
+/**
+ * struct code_format - strucure format
+ * @sc: specifier
+ * @f: func
+ */
+typedef struct code_format
+{
+char *sc;
+int (*f)(va_list);
+} code_f;
 
 #endif
