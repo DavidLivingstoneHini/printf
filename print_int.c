1510660db65e83122b8922aa45002849df2ff0b0
Biniyam Tamene <binitame123@gmail.com>
2021-07-28 08:10:37 -0700

    Printf Commit

--- print_int.c
+++ print_int.c
@@ -1,37 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 #include "holberton.h"
 
 /**
- * printf_bin - prints the binary number
- * @list: argument
- * Return: count;
+ * print_int - prints decemal 
+ * @d: the dec
+ * Return: 1
  */
 
-int printf_bin(va_list list)
+int print_int(va_list i)
 {
-int flag = 0;
-int count = 0;
-int n = va_arg(list, int);
-int p;
-int i, x = 1, y;
-for (i = 0; i < 32; i++)
-{
-p = ((x << (31 - i)) & n);
-if (p >> (31 - i))
-flag = 1;
-if (flag)
-{
-y = p >> (31 - i);
-_putchar(y + 48);
-count++;
-}
-}
-if (count == 0)
-{
-count++;
-putchar('0');
-}
-return (count);
+va_arg(i, int);
+return (1);
 }
