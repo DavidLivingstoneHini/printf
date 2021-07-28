f73dcef1e36c6bd12b98611677bc232addb76ca1
Biniyam Tamene <binitame123@gmail.com>
2021-07-28 07:39:36 -0700

    Printf Commit

---
+++ print_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * print_string - prints string character
+ * @s: string to be printed
+ * Return: i
+ */
+
+int print_string(va_list s)
+{
+char *my_str;
+int i = 0;
+my_str = va_arg(s, char *);
+if (my_str == NULL)
+my_str = "(null)";
+while (my_str[i])
+{
+_putchar(my_str[i]);
+i++;
+}
+return (i);
+}
