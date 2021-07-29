f6dcab563c46ab13d078faab3741cee47c3b5a75
Biniyam Tamene <binitame123@gmail.com>
2021-07-29 09:20:23 -0700

    Printf Commit

---
+++ _printf.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * find_correct_func - finding
+ * @format: format
+ */
+
+int (*find_correct_func(const char *format))(va_list)
+{
+unsigned int i = 0;
+code_f find_f[] = {
+{"c", print_char},
+{"s", print_string},
+{NULL, NULL}
+};
+
+while (find_f[i].sc)
+{
+if (find_f[i].sc[0] == (*format))
+return (find_f[i].f);
+i++;
+}
+return (NULL);
+}
+
+/**
+ * _printf - produces an output based on format
+ * @format: format
+ * Return: size
+ */
+int _printf(const char *format, ...)
+{
+va_list list;
+int (*f)(va_list);
+unsigned int i = 0, len = 0;
+if (format == NULL)
+return (-1);
+va_start(list, format);
+while (format[i])
+{
+while (format[i] != '%' && format[i])
+{
+_putchar(format[i]);
+len++;
+i++;
+}
+if (format[i] == '\0')
+return (len);
+f = find_correct_func(&format[i + 1]);
+if (f != NULL)
+{
+len += f(list);
+i += 2;
+continue;
+}
+if (!format[i + 1])
+return (-1);
+_putchar(format[i]);
+len++;
+if (format[i + 1] == '%')
+i += 2;
+else
+i++;
+}
+va_end(list);
+return (len);
+}
