fd8f61db8f8cb9a1b66eedce99d9c669df3e9f52
Biniyam Tamene <binitame123@gmail.com>
2021-07-29 01:45:37 -0700

    Printf Commit

--- print_dec.c
+++ print_dec.c
@@ -10,6 +10,42 @@
 
 int print_dec(va_list d)
 {
-va_arg(d, int);
+int len, pow, j, digit, n, count, num;
+count = 0;
+n = va_arg(d, int);
+if (n != 0)
+{
+if (n < 0)
+{
+_putchar('-');
+count++;
+}
+num = n;
+len = 0;
+while (num != 0)
+{
+num /= 0;
+len++;
+}
+pow = 1;
+for (j = 1; j <= len - 1; j++)
+pow *= 10;
+for (j = 1; j <= len; j++)
+{
+digit = n / pow;
+if (n < 0)
+_putchar((digit * -1) + 48);
+else
+_putchar(digit + '0');
+count++;
+n -= (digit * pow);
+pow /= 10;
+}
+}
+else
+{
+_putchar('0');
 return (1);
+}
+return (count);
 }
--- print_int.c
+++ print_int.c
@@ -3,13 +3,49 @@
 #include "holberton.h"
 
 /**
- * print_int - prints decemal 
+ * print_dec - prints decemal 
  * @d: the dec
  * Return: 1
  */
 
 int print_int(va_list i)
 {
-va_arg(i, int);
+int len, pow, j, digit, n, count, num;
+count = 0;
+n = va_arg(i, int);
+if (n != 0)
+{
+if (n < 0)
+{
+_putchar('-');
+count++;
+}
+num = n;
+len = 0;
+while (num != 0)
+{
+num /= 0;
+len++;
+}
+pow = 1;
+for (j = 1; j <= len - 1; j++)
+pow *= 10;
+for (j = 1; j <= len; j++)
+{
+digit = n / pow;
+if (n < 0)
+_putchar((digit * -1) + 48);
+else
+_putchar(digit + '0');
+count++;
+n -= (digit * pow);
+pow /= 10;
+}
+}
+else
+{
+_putchar('0');
 return (1);
+}
+return (count);
 }
