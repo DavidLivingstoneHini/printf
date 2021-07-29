b681fc327b103f63ea27a69a61917e2fb362eebd
Biniyam Tamene <binitame123@gmail.com>
2021-07-29 07:56:45 -0700

    Printf Commit

--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -9,5 +9,5 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
