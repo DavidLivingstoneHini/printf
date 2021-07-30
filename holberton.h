0a11284962800281ce66e8e254710d85930b7a23
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-07-30 19:12:12 +0000

    Create holberton.h

--- holberton.h
+++ holberton.h
@@ -17,7 +17,7 @@ int print_unsigned(va_list u);
 int print_rot13(va_list R);
 int print_rev(va_list r);
 int print_int(va_list i);
-int print_S(va_list *list);
+int print_S(va_list S);
 
 /**
  * struct code_format - structure format
