1f7033169b50793d84d5a84184685df7c767aa6d
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-07-30 20:51:44 +0000

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -19,7 +19,6 @@ int print_rot13(va_list R);
 int print_rev(va_list r);
 int print_int(va_list i);
 int print_S(va_list S);
-int print_address(va_list p);
 
 /**
  * struct code_format - structure format
