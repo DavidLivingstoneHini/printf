638415dfd5970ac7eb8cb291bf76596e8fa7817d
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-07-30 11:24:24 +0000

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -4,20 +4,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-/**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
- */
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
-
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list c);
