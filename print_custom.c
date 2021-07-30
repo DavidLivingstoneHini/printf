2d304ba792764628f6b2ca03c2c35f8ec6985cdc
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-07-30 14:38:56 +0000

    Create print_custom.c

---
+++ print_custom.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+  * print_S - prints custom specifier
+  * @list: va_list
+  * Return: number of characters printed
+  */
+int print_S(va_list *list)
+{
+	int count = 0, i = 0;
+	char *str = va_arg(*list, char *);
+	char c;
+
+	if (str == NULL || *str == '\0')
+		return (0);
+
+	while (str[i])
+	{
+		c = str[i];
+		if ((c > 0 && c < 32) || c >= 127)
+		{
+			count += _putchar('\\');
+			count += _putchar('x');
+			count += _putchar('0');
+			count += _printf("%X", (unsigned int) c);
+		}
+		else
+		{
+			count += _putchar(c);
+		}
+		i++;
+	}
+	return (count);
+}
