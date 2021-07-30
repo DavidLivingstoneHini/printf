ca9780d037bd167691cad0d5bb551297c58155e2
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-07-30 21:44:44 +0000

    Update print_cus.c

--- print_cus.c
+++ print_cus.c
@@ -7,28 +7,28 @@
   */
 int print_S(va_list S)
 {
-	int count = 0, i = 0;
-	char *str = va_arg(S, char *);
-	char c;
+int count = 0, i = 0;
+char *str = va_arg(S, char *);
+char c;
 
-	if (str == NULL || *str == '\0')
-		return (0);
+if (str == NULL || *str == '\0')
+return (0);
 
-	while (str[i])
-	{
-		c = str[i];
-		if ((c > 0 && c < 32) || c >= 127)
-		{
-			count += _putchar('\\');
-			count += _putchar('x');
-			count += _putchar('0');
-			count += _printf("%X", (unsigned int) c);
-		}
-		else
-		{
-			count += _putchar(c);
-		}
-		i++;
-	}
-	return (count);
+while (str[i])
+{
+c = str[i];
+if ((c > 0 && c < 32) || c >= 127)
+{
+count += _putchar('\\');
+count += _putchar('x');
+count += _putchar('0');
+count += _printf("%X", (unsigned int) c);
+}
+else
+{
+count += _putchar(c);
+}
+i++;
+}
+return (count);
 }
