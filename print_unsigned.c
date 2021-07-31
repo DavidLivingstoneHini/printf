f0f3fcab3af5586c0e35c5ce31fc5d7d4f48e4eb
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-07-31 00:18:58 +0000

    Update print_unsigned.c

--- print_unsigned.c
+++ print_unsigned.c
@@ -1,44 +1,97 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
 #include "holberton.h"
 
 /**
- * print_unsigned - prints unsigned
- * @d: unsigned to be printed
- * Return: size
+ * print_u - prints an unsigned in in decimal notation
+ * @u: unsigned int to print
+ *
+ * Return: number of digits printed
  */
-
 int print_unsigned(va_list u)
 {
-unsigned int len, pow, j, digit, n, num;
-int count = 0;
-n = va_arg(u, unsigned int);
-if (n != 0)
-{
-num = n;
-len = 0;
-while (num != 0)
-{
-num /= 10;
-len++;
+	unsigned int a[10];
+	unsigned int i, m, n, sum;
+	int count;
+
+	n = va_arg(u, unsigned int);
+	m = 1000000000; /* (10 ^ 9) */
+	a[0] = n / m;
+	for (i = 1; i < 10; i++)
+	{
+		m /= 10;
+		a[i] = (n / m) % 10;
+	}
+	for (i = 0, sum = 0, count = 0; i < 10; i++)
+	{
+		sum += a[i];
+		if (sum || i == 9)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
 }
-pow = 1;
-for (j = 1; j <= len - 1; j++)
-pow *= 10;
-for (j = 1; j <= len; j++)
+
+/**
+ * print_o - takes an unsigned int and prints it in octal notation
+ * @o: unsigned int to print
+ *
+ * Return: number of digits printed
+ */
+int print_octal(va_list o)
 {
-digit = n / pow;
-_putchar(digit + '0');
-count++;
-n -= (digit * pow);
-pow /= 10;
+	unsigned int a[11];
+	unsigned int i, m, n, sum;
+	int count;
+
+	n = va_arg(o, unsigned int);
+	m = 1073741824; /* (8 ^ 10) */
+	a[0] = n / m;
+	for (i = 1; i < 11; i++)
+	{
+		m /= 8;
+		a[i] = (n / m) % 8;
+	}
+	for (i = 0, sum = 0, count = 0; i < 11; i++)
+	{
+		sum += a[i];
+		if (sum || i == 10)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
 }
-}
-else
+
+/**
+ * print_b - takes an unsigned int and prints it in binary notation
+ * @b: unsigned in to print
+ *
+ * Return: number of digits printed
+ */
+int print_bin(va_list b)
 {
-_putchar('0');
-return (1);
-}
-return (count);
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+
+	n = va_arg(b, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
 }
